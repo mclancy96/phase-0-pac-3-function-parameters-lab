4f9b3319fd70ec3b28ed7c5325b8d6bccedd7c0f
Mike Clancy <mclancy96@gmail.com>
2025-02-18 21:59:18 -0500

    add introductionWithLanguage

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const introduction = (name) => {
     return `Hi, my name is ${name}.`
 }
+
+const introductionWithLanguage = (name, language) => {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
